ccaecafff2ad32d55d92ce6363be487ab25c72eb
Thomas Herrell <wolfmailman@yahoo.com>
2022-04-21 07:47:00 -0400

    readmes, project2 almost done

--- project2/checkheaders.py
+++ project2/checkheaders.py
@@ -1,6 +1,13 @@
 ''' Header security checker '''
 import argparse
 import requests
+from colorama import Fore, Style
+
+tests = [
+    "Strict-Transport-Security",
+    "Content-Security-Policy",
+    "X-Frame-Options"
+]
 
 def check_urls(urls):
     ''' check each url is HTTPS, add to list if so '''
@@ -23,22 +30,25 @@ def main():
     if args.URL:
         urls = check_urls([args.URL])
     elif args.inFile:
-        file = open(args.inFile, encoding = 'utf8')
-        urls = check_urls(file.readlines())
+        with open(args.inFile, encoding = 'utf8') as file:
+            urls = check_urls(file.readlines())
 
-    print("Starting Strict-Transport-Security test\n")
     if len(urls) > 0:
-        try:
-            for url in urls:
+        for url in urls:
+            try:
                 response = requests.get(url)
                 #print(response.headers)
-                if response.headers["Strict-Transport-Security"]:
-                    print(url + " passed!")
-        except KeyError:
-            print(url + " failed!")
-        except Exception as xcpt:
-            print(f"Exception: {type(xcpt).__name__}")
-            print(f"Exception message: {xcpt}")
+                print(url + ":")
+                for test in tests:
+                    try:
+                        if response.headers[test]:
+                            print(f"\t{test:<35} {Fore.GREEN}PASSED{Style.RESET_ALL}")
+                    except KeyError:
+                        print(f"\t{test:<35} {Fore.RED}FAILED{Style.RESET_ALL}")
+                        continue
+            except Exception as xcpt:
+                print(f"Exception: {type(xcpt).__name__}")
+                print(f"Exception message: {xcpt}")
 
     print("\nDone!")
 
